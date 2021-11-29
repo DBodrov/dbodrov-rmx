23c155fa65f9bd1eae26efe3bfad828992940fe5
d.bodrov <bodrov@a-3.ru>
2021-11-29 19:01:12 +0300

    clean up

--- app/api/auth.api.ts
+++ app/api/auth.api.ts
@@ -2,19 +2,14 @@ export async function readPublicToken(request: Request) {
   try {
     const {protocol, host} = new URL(request.url);
     const SERVER_URL = `${protocol}//${host}`
-    console.log('SERVER_URL :', SERVER_URL)
     const response = await fetch(`${SERVER_URL}/api/getPublicToken`);
-    if (! response.ok) {
+    if (!response.ok) {
       throw new Response('token lambda error', {status: 400})
     }
-    console.log('api response', response)
     const token = await response.json();
-    
-    console.log('try readPubToken', token.data)
-    //console.log(token)
     return token;
   } catch (error) {
     console.log(error)
-    throw new Response('No token', {status: 500});
+    throw new Response('No token', {status: 400});
   }
 }
--- app/routes/lk/index.tsx
+++ app/routes/lk/index.tsx
@@ -11,22 +11,16 @@ export const links: LinksFunction = () => {
 export const action: ActionFunction = async ({ request, context, params }) => {
   const formData = await request.formData();
   const amount = formData.get("amount");
-  console.log("amount is ", amount);
-  //const url = globalThis.location.host;
-  console.log(request.url);
   if (!amount) {
     return json({ error: "Укажите сумму" }, { status: 400 });
   }
-  // const res = await fetch('http://localhost:3000/api/getPublicToken');
   const publicToken = await readPublicToken(request);
-  // paidservice_id=4244&LS=100340011200&_SUM=2.00&region_id=1022&ADDRESS=%D0%B3+%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%2C+%D0%BF%D1%80%D0%BE%D0%B5%D0%B7%D0%B4+%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%BD%D0%B0%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%2C+%D0%B4.6%D0%B4%D0%BE%D0%B314080+%D0%BA%D0%B2.7&url=https%3A%2F%2Flk.karelia.tns-e.ru%2Flk-pay&pay=true&channel=2&lk=&jwt=__super__token__
   const paymentUrl = `https://localhost:6333/4244?paidservice_id=4244&LS=100340011200&_SUM=${amount}&region_id=1022&ADDRESS=%D0%B3+%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%2C+%D0%BF%D1%80%D0%BE%D0%B5%D0%B7%D0%B4+%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%BD%D0%B0%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%2C+%D0%B4.6%D0%B4%D0%BE%D0%B314080+%D0%BA%D0%B2.7&url=https%3A%2F%2Flk.karelia.tns-e.ru%2Flk-pay&pay=true&channel=2&lk=&jwt=${publicToken.data}`;
   return json({ data: paymentUrl }, 200);
 };
 
 export default function LKPage() {
   const actionData = useActionData();
-  console.log(actionData);
 
   React.useEffect(() => {
     if (actionData?.data) {
