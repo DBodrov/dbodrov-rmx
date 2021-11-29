24846dd8d1cabf484b3172104530c534eaf22e91
d.bodrov <bodrov@a-3.ru>
2021-11-29 14:40:20 +0300

    log to api

--- netlify/functions/getPublicToken.js
+++ netlify/functions/getPublicToken.js
@@ -17,6 +17,7 @@ exports.handler = async (event, context) => {
       }
     );
     const token = await response.json();
+    console.log('TOKEN :', token)
     return {
       statusCode: 200,
       body: JSON.stringify({
@@ -24,6 +25,7 @@ exports.handler = async (event, context) => {
       }),
     };
   } catch (err) {
+    console.log(err)
     return {
       statusCode: err.statusCode || 500,
       body: JSON.stringify({
