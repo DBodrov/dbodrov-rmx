250f04f0d2c34cbc3b4dc858da7e813d3dd40dff
d.bodrov <bodrov@a-3.ru>
2021-11-29 18:39:18 +0300

    fix api 2

--- app/api/auth.api.ts
+++ app/api/auth.api.ts
@@ -4,12 +4,17 @@ export async function readPublicToken(request: Request) {
     const SERVER_URL = `${protocol}//${host}`
     console.log('SERVER_URL :', SERVER_URL)
     const response = await fetch(`${SERVER_URL}/api/getPublicToken`);
+    if (! response.ok) {
+      throw new Response('token lambda error', {status: 400})
+    }
+    console.log('api response', response)
     const token = await response.json();
     
-    console.log('try readPubToken', token)
-    console.log(token)
+    console.log('try readPubToken', token.data)
+    //console.log(token)
     return token;
   } catch (error) {
+    console.log(error)
     throw new Response('No token', {status: 500});
   }
 }
--- netlify/functions/getPublicToken.js
+++ netlify/functions/getPublicToken.js
@@ -1,36 +1,11 @@
-const fetch = require("node-fetch");
+const uuid = require('uuid');
 
 exports.handler = async (event, context) => {
-  try {
-    console.log('get token API')
-    const response = await fetch(
-      "https://www.uuidtools.com/api/generate/v1",
-      {
-        headers: {
-          accept: "application/json",
-          "cache-control": "no-cache",
-        },
-        body: null,
-        method: "GET",
-        mode: "cors",
-        credentials: "omit",
-      }
-    );
-    const token = await response.json();
-    console.log('TOKEN :', token)
+    const token = uuid.v4();
+    console.log('token :', token)
+
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        data: token,
-      }),
-    };
-  } catch (err) {
-    console.log(err)
-    return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message,
-      }),
-    };
-  }
+      body: JSON.stringify({data: token})
+    }
 };
