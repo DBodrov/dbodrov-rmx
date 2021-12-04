04faafeb6f0879628b96d06ae3c2e80187031c97
d.bodrov <bodrov@a-3.ru>
2021-12-04 18:24:55 +0300

    add return url

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -14,9 +14,14 @@ export const action: ActionFunction = async ({ request, context, params }) => {
   if (!amount || isNaN(Number(amount))) {
     return json({ error: "Укажите сумму" }, { status: 400 });
   }
+  const returnUrl = decodeURIComponent(request.url);
+
   const publicToken = await readPublicToken(request);
-  const PAYMENT_HOST = process.env.NODE_ENV === 'development' ? 'https://localhost:6333' : 'https://payment-t.netlify.app';
-  const paymentUrl = `${PAYMENT_HOST}/4244?paidservice_id=4244&LS=100340011200&_SUM=${amount}&region_id=1022&ADDRESS=%D0%B3+%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%2C+%D0%BF%D1%80%D0%BE%D0%B5%D0%B7%D0%B4+%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%BD%D0%B0%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%2C+%D0%B4.6%D0%B4%D0%BE%D0%B314080+%D0%BA%D0%B2.7&url=https%3A%2F%2Flk.karelia.tns-e.ru%2Flk-pay&pay=true&channel=2&lk=&jwt=${publicToken.data}`;
+  const PAYMENT_HOST =
+    process.env.NODE_ENV === "development"
+      ? "https://localhost:6333"
+      : "https://payment-t.netlify.app";
+  const paymentUrl = `${PAYMENT_HOST}/4244?paidservice_id=4244&LS=100340011200&_SUM=${amount}&region_id=1022&ADDRESS=%D0%B3+%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B7%D0%B0%D0%B2%D0%BE%D0%B4%D1%81%D0%BA%2C+%D0%BF%D1%80%D0%BE%D0%B5%D0%B7%D0%B4+%D0%A1%D0%BA%D0%B0%D0%BD%D0%B4%D0%B8%D0%BD%D0%B0%D0%B2%D1%81%D0%BA%D0%B8%D0%B9%2C+%D0%B4.6%D0%B4%D0%BE%D0%B314080+%D0%BA%D0%B2.7&url=${returnUrl}&pay=true&channel=2&lk=&jwt=${publicToken.data}`;
   return json({ data: paymentUrl }, 200);
 };
 
@@ -32,12 +37,21 @@ export default function LKPage() {
 
   return (
     <section>
-      
       <h1>Личный кабинет</h1>
       <Form method="post" className="form">
         <label htmlFor="amount">Сумма платежа</label>
-        <input autoComplete="off" className={`amount-input ${hasError && 'input-error'}`} type="tel" name="amount" id="amount" />
-        {hasError ? <span className="error-text" role="alert">{actionData.error}</span> : null}
+        <input
+          autoComplete="off"
+          className={`amount-input ${hasError && "input-error"}`}
+          type="tel"
+          name="amount"
+          id="amount"
+        />
+        {hasError ? (
+          <span className="error-text" role="alert">
+            {actionData.error}
+          </span>
+        ) : null}
         <button type="submit" className="submit-button">
           Оплатить
         </button>
